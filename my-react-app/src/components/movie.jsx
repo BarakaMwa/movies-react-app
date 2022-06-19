b7fb88f2aff5f4ffddeaafe898f0a965778d0b0b
baraka mwangamba <mwangamb@zra.org.zm>
2022-06-19 14:16:37 +0200

    card Animation and clean up

--- my-react-app/src/components/movie.jsx
+++ my-react-app/src/components/movie.jsx
@@ -8,7 +8,7 @@ function Movie(prop) {
     const LikeMovie = prop.likeMovie;
 
     return (
-        <div className="card p-3 col-12 col-sm-6 col-md-6 col-lg-2 col-xl-2">
+        <div className="card p-3 col-12 col-sm-6 col-md-6 col-lg-2 col-xl-2 mt-3 mb-3">
             <div className="card-wrapper align-content-center animate__animated pb-0">
                 <div className="card-img">
                     <img src={"https://image.tmdb.org/t/p/w300" + prop.poster_path} alt="images"/>
